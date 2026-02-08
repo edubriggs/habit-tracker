8171b07cb04428878404d7787c4001735c6175b8
edubriggs <edubriggs@gmail.com>
2026-02-08 16:22:40 -0300

    tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{config('app.name')}}</title>
 
-<h1>Welcome to the home page</h1>
-
-<p>Olá {{ $name }}</p>
-<p>Seus hábitos são:</p>
-<ul>
-    @foreach ($habits as $item )
-        <li>
-            {{ $item }}
-        </li>
-    @endforeach
-</ul>
-
-@auth
-    <p>Você está logado!</p>
-@endauth
-
-@guest
-    <p>Você não está logado!</p>
-@endguest
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <h1 class="text-3x1 font-bold underline text-blue-900">
+            Hello World!
+        </h1>
+    </body>
+</html>
