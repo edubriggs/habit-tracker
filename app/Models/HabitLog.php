f385430f19649a5d3d89163cf1f8cf85c5028517
edubriggs <edubriggs@gmail.com>
2026-02-17 11:35:42 -0300

    Eloquent

--- app/Models/HabitLog.php
+++ app/Models/HabitLog.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class HabitLog extends Model
 {
         protected $fillable = [
@@ -11,4 +11,12 @@ class HabitLog extends Model
        'habit_id',
        'completed_at',
     ];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function habit(): BelongsTo{
+        return $this->belongsTo(Habit::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,7 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -42,5 +42,15 @@ class User extends Authenticatable
         return [
             'password' => 'hashed',
         ];
+    }
+
+    //Um usuario pode ter varios hábitos
+    public function habits(): HasMany{
+        return $this->hasMany(Habit::class);
+    }
+
+    //Um usuario pode ter varios logs
+    public function habitsLogs(): HasMany{
+        return $this->hasMany(HabitLog::class);
     }
 }
