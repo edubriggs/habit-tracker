8c0234d5db481cd113c064def1d9e18a5fc50858
edubriggs <edubriggs@gmail.com>
2026-03-07 00:13:42 -0300

    grid contribuições

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,42 +1,45 @@
-<header class="bg-white border-bottom border-b-2 flex item-center justify-between p-4">
+<header class="bg-white border-bottom border-b-2">
     <!-- LOGO -->
-    
-    <div class="flex items-center gap-2">
-        <a href="{{ route('habits.index') }}"class="habit-btn habit-shadow-lg px-2 py-1 bg-habit-orange">
-            HT
-        </a>
-        <p>
-            Habit Tracker
-        </p>
-    </div>
+    <div class="max-w-7xl mx-auto flex items-center justify-between p-4">
 
-    
-    <div>
+        <div class="flex items-center gap-2">
+            <a href="{{ route('habits.index') }}"class="habit-btn habit-shadow-lg px-2 py-1 bg-habit-orange">
+                HT
+            </a>
+            <p>
+                Habit Tracker
+            </p>
+        </div>
 
-        @auth
-            <form class="inline"action="{{ route('auth.logout') }}" method="POST">
-                @csrf
-
-                <button type="submit" class="bg-habit-orange p-2 habit-btn habit-shadow-lg hover:opacity-70">
-                    Sair
-                </button>
-                
-           
-            </form>
         
-        @endauth
-        
+        <div>
 
-        @guest
-            <div class="flex gap-2">
-                <a href="{{ route('site.register') }}" class=" p-2 habit-btn habit-shadow-lg hover:opacity-70">
-                    Cadastrar
-                </a>
-                <a href="{{ route('site.login') }}" class="bg-habit-orange p-2 habit-btn habit-shadow-lg hover:opacity-70">
-                    Logar
-                </a>
-            </div>
+            @auth
+                <form class="inline"action="{{ route('auth.logout') }}" method="POST">
+                    @csrf
+
+                    <button type="submit" class="bg-habit-orange p-2 habit-btn habit-shadow-lg hover:opacity-70">
+                        Sair
+                    </button>
+                    
             
-        @endguest
+                </form>
+            
+            @endauth
+            
+
+            @guest
+                <div class="flex gap-2">
+                    <a href="{{ route('site.register') }}" class=" p-2 habit-btn habit-shadow-lg hover:opacity-70">
+                        Cadastrar
+                    </a>
+                    <a href="{{ route('site.login') }}" class="bg-habit-orange p-2 habit-btn habit-shadow-lg hover:opacity-70">
+                        Logar
+                    </a>
+                </div>
+                
+            @endguest
+        </div>
+
     </div>
 </header>
